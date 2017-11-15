5bcdefb6a133609f74d5afd2b59114a655b05744
alex <alex@rocketguys.com>
2017-11-15 11:35:34 +0300

    fix vol listeners

--- index.js
+++ index.js
@@ -184,10 +184,10 @@ app.hears(/^(?:(?:(?:сы|и)грай|воспроизведи|play)\s+((?:.|\n)
  vol
 */
 
-app.hears(/^(?:(?:сделай\s+)?(?:по)?громче|make(\s+(?:sound|music))?\s+louder\s*(?:до|up\s*to)\s+(\d+))/i, (ctx) => {
+app.hears(/^(?:(?:сделай\s+)?(?:по)?громче|make(\s+(?:sound|music))?\s+louder)\s+(?:до|up\s*to)\s+(\d+)/i, (ctx) => {
   commands.run('vol', 'upTo', ctx)
 })
-app.hears(/^(?:(?:сделай\s+)?(?:по)?тише|make(\s+(?:sound|music))?\s+quieter\s*(?:до|down\s*to)\s+(\d+))/i, (ctx) => {
+app.hears(/^(?:(?:сделай\s+)?(?:по)?тише|make(\s+(?:sound|music))?\s+quieter)\s+(?:до|down\s*to)\s+(\d+)/i, (ctx) => {
   commands.run('vol', 'downTo', ctx)
 })
 app.hears(/^(?:(?:сделай\s+)?(?:по)?тише|make(?:\s+(?:sound|music))?\s+quieter)/i, (ctx) => {
