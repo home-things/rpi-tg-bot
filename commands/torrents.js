44c1db3aa96d9f791e49567df5efc98c45464457
alex <alex@rocketguys.com>
2017-12-31 00:21:04 +0300

    fix

--- commands/torrents.js
+++ commands/torrents.js
@@ -1,33 +1,36 @@
+const bindAll = require('lodash.bindall')
 const { UserError, exec, openRpi3, unindent } = require('../src/common')
 
-module.exports = () => ({
+module.exports = () => bindAll({
   async search (query) {
     return JSON.parse(await exec(`search-rutracker ${ query }`)) || []
   },
 
-  printable: (torrent) => unindent`
+  printable (torrent) { return unindent`
     📕 ${ torrent.category }.
     <b>${ torrent.size_h }</b>. seeds: <b>${ torrent.seeds }</b> / leechs: ${ torrent.leechs }
     ${ torrent.title } <b> # ${ torrent.id }</b>
-  `,
+  `},
 
-  download: (id) => exec(`download-rutracker ${ id }`),
+  download (id) {
+    return exec(`download-rutracker ${ id }`)
+  },
 
-  downloading: async () => {
+  async downloading () {
     return Boolean(await this.info())
   },
 
-  status: async () => {
+  async status () {
     const info = this.info()
     return info ? `${ await info }\nОстальное скачалось` : 'Всё скачалось, господа'
   },
 
-  info: async () => {
+  async info () {
     const info = await openRpi3('deluge-console info -s Downloading --sort=time_added')
     return info.replace(/^(ID|State|Seeds|Seed time|Tracker status|Size):.+\n/gm, "").trim()
   },
 
-  awaitDownloaded: async () => {
+  async awaitDownloaded () {
     if (this.isDownloadedPending) return this.isDownloadedPending
 
     this.isDownloadedPending = new Promise(async (res) => {
