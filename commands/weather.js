24018c230c4b0ac7e2f706c85f1d1a430a9fddc6
Alex Mextner <mxtnr@Alexs-MacBook-Pro.local>
2018-07-09 09:43:31 +0300

    weather: add eveningTemp

--- commands/weather.js
+++ commands/weather.js
@@ -7,8 +7,10 @@ module.exports = () => ({
     const weather = await exec('get-weather2').then(res => JSON.parse(res))
 
     const temp = Math.floor(weather.temp)
+    const eveningTemp = Math.floor(weather.eveningTemp)
     if (!weather.description || !weather.temp) throw new Error('no_data')
-    const formattedWeather = `Погода в ближайшее время: ${ weather.description || '' } ${ temp } ${ getUnit(temp) }`
+    const formattedWeather = `Погода в ближайшее время: ${ weather.description || '' } ${ temp } ${ getUnit(temp) }.
+    А вечером:  ${ eveningTemp } ${ getUnit(eveningTemp) }`
 
     return formattedWeather
 
